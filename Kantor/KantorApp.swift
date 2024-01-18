1d8f968535df05546d9a7a30d55c1ed9843c797c
Pavel Markitantov <markitantow@gmail.com>
2024-01-18 17:17:00 +0100

    Initial Commit

---
+++ Kantor/KantorApp.swift
@@ -0,0 +1,17 @@
+//
+//  KantorApp.swift
+//  Kantor
+//
+//  Created by Pavel Markitantov on 18/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct KantorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
